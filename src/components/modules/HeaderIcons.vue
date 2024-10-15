242c243a26527fbe732ff5507738db5b5007e87b
Janos Perge <janos.perge@gmail.com>
2024-10-15 16:28:22 +0200

    add icons

---
+++ src/components/modules/HeaderIcons.vue
@@ -0,0 +1,24 @@
+<script setup>
+import iconHeart from '@/components/icons/commons/iconHeart.vue'
+import iconCart from '@/components/icons/commons/iconCart.vue'
+import iconUser from '@/components/icons/commons/iconUser.vue'
+</script>
+<template>
+  <div class="header-icons">
+    <button>
+      <iconHeart />
+    </button>
+    <button>
+      <iconCart />
+    </button>
+    <button>
+      <iconUser />
+    </button>
+  </div>
+</template>
+<style scoped>
+.header-icons {
+  display: flex;
+  gap: 24px;
+}
+</style>
