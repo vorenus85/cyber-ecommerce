bdc2fe15449fad8c318487a92ae381405877abb8
Janos Perge <janos.perge@gmail.com>
2024-10-15 15:44:09 +0200

    make dinamic footer menus

--- src/components/modules/FooterInformation.vue
+++ src/components/modules/FooterInformation.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="footer-information module">
     <div class="module-head">
-      <h3 class="module-head-title">Module head</h3>
+      <h3 class="module-head-title">{{ title }}</h3>
     </div>
     <div class="module-body">
       <ul class="footer-information-list">
-        <li class="footer-information-item">
-          <a href="#" class="footer-information-link">Lorem ipsum dolor</a>
-        </li>
-        <li class="footer-information-item">
-          <a href="#" class="footer-information-link">Lorem ipsum dolor</a>
-        </li>
-        <li class="footer-information-item">
-          <a href="#" class="footer-information-link">Lorem ipsum dolor</a>
-        </li>
-        <li class="footer-information-item">
-          <a href="#" class="footer-information-link">Lorem ipsum dolor</a>
-        </li>
-        <li class="footer-information-item">
-          <a href="#" class="footer-information-link">Lorem ipsum dolor</a>
-        </li>
-        <li class="footer-information-item">
-          <a href="#" class="footer-information-link">Lorem ipsum dolor</a>
+        <li class="footer-information-item" v-for="menu in menus" :key="menu.id">
+          <a :href="menu.url" class="footer-information-link">{{ menu.name }}</a>
         </li>
       </ul>
     </div>
   </div>
 </template>
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'Module'
+  },
+  menus: {
+    type: Array
+  }
+})
+</script>
 <style scoped>
 .module-head {
   color: #fff;
@@ -40,5 +36,6 @@
 .footer-information-link {
   color: #cfcfcf;
   font-size: 0.875rem;
+  font-weight: 300;
 }
 </style>
--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -1,23 +1,46 @@
 <script setup>
+import { ref } from 'vue'
 import FooterCustom from '@/components/modules/FooterCustom.vue'
 import Socials from '@/components/modules/Socials.vue'
 import FooterInformation from '@/components/modules/FooterInformation.vue'
+
+const servicesTitle = 'Services'
+const servicesMenus = ref([
+  { name: 'Bonus program', id: '1', url: '#' },
+  { name: 'Gift cards', id: '2', url: '#' },
+  { name: 'Credit and payment', id: '3', url: '#' },
+  { name: 'Service contracts', id: '4', url: '#' },
+  { name: 'Non-cash account', id: '5', url: '#' },
+  { name: 'Payment', id: '6', url: '#' }
+])
+
+const assistanceTitle = 'Assistance to the buyer'
+const assistanceMenus = ref([
+  { name: 'Find an order', id: '1', url: '#' },
+  { name: 'Terms of delivery', id: '2', url: '#' },
+  { name: 'Exchange and return of goods', id: '3', url: '#' },
+  { name: 'Guarantee', id: '4', url: '#' },
+  { name: 'Frequently asked questions', id: '5', url: '#' },
+  { name: 'Terms of use of the site', id: '6', url: '#' }
+])
 </script>
 <template>
   <section class="footer">
     <div class="container mx-auto">
-      <div class="footer-rows grid grid-cols-3 gap-4">
+      <div class="footer-rows grid grid-cols-1 md:grid-cols-3 gap-4">
         <div class="footer-column footer-column-1">
           <FooterCustom />
-          <Socials />
         </div>
         <div class="footer-column footer-column-2">
-          <FooterInformation />
+          <FooterInformation :title="servicesTitle" :menus="servicesMenus" />
         </div>
 
         <div class="footer-column footer-column-2">
-          <FooterInformation />
+          <FooterInformation :title="assistanceTitle" :menus="assistanceMenus" />
         </div>
+      </div>
+      <div class="mt-4">
+        <Socials />
       </div>
     </div>
   </section>
@@ -37,5 +60,14 @@ import FooterInformation from '@/components/modules/FooterInformation.vue'
 <style>
 .footer .logo {
   color: #fff;
+}
+
+.footer .module {
+  margin-bottom: 0;
+}
+
+.footer .module-head {
+  font-size: 1rem;
+  font-weight: 600;
 }
 </style>
