1a461dd757a8d5d8e48e58bf93f10694aaa4f948
Janos Perge <janos.perge@gmail.com>
2024-10-18 22:54:50 +0200

    extend productAccordion module

--- src/components/sections/ProductAccordions.vue
+++ src/components/sections/ProductAccordions.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="product-accordions container">
+    <div class="product-accordion-menu mb-8">
+      <ul class="flex gap-6 md:gap-8">
+        <li
+          @click="fetchProducts('newArrival')"
+          :class="[isActive === 'newArrival' ? 'selected' : '']"
+        >
+          New Arrival
+        </li>
+        <li
+          @click="fetchProducts('bestSeller')"
+          :class="[isActive === 'bestSeller' ? 'selected' : '']"
+        >
+          Bestseller
+        </li>
+        <li @click="fetchProducts('featured')" :class="[isActive === 'featured' ? 'selected' : '']">
+          Featured Products
+        </li>
+      </ul>
+    </div>
+
     <ProductModule :error="error" :loading="loading" :products="products" />
   </div>
 </template>
@@ -15,33 +35,71 @@ export default {
         .fill()
         .map(() => [])
     )
+    const isActive = ref('newArrival')
     const loading = ref(true)
     const error = ref(null)
 
-    const fetchProducts = async () => {
+    const fetchProducts = async (productType) => {
+      loading.value = true
+
       try {
         const response = await fetch(import.meta.env.VITE_PRODUCTS_ENDPOINT)
         if (!response.ok) {
           throw new Error('Failed to load products')
         }
         const data = await response.json()
-        products.value = data.slice(0, limit)
+        // mocking newArrival | bestSeller | featured
+        if (productType === 'bestSeller') {
+          products.value = data.slice(4, limit + 4)
+        } else if (productType === 'featured') {
+          products.value = data.slice(6, limit + 6)
+        } else {
+          products.value = data.slice(0, limit)
+        }
       } catch (err) {
         error.value = err.message
       } finally {
+        isActive.value = productType
         loading.value = false
       }
     }
 
     onMounted(() => {
-      fetchProducts()
+      fetchProducts('newArrival')
     })
 
     return {
       products,
+      fetchProducts,
       loading,
-      error
+      error,
+      isActive
     }
   }
 }
 </script>
+<style>
+.product-accordion-menu li {
+  color: #8b8b8b;
+  font-weight: 500;
+  cursor: pointer;
+  position: relative;
+  transition: var(--transition);
+}
+
+.product-accordion-menu li:hover,
+.product-accordion-menu li.selected {
+  color: #000;
+}
+
+.product-accordion-menu li.selected:after {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: -3px;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background: #000;
+}
+</style>
