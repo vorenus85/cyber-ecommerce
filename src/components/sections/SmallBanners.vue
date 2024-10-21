70a250b4853e33d6f3b9ffab78bcf94a11000c12
Janos Perge <janos.perge@gmail.com>
2024-10-21 10:54:56 +0200

    fix small banners alt

--- src/components/sections/SmallBanners.vue
+++ src/components/sections/SmallBanners.vue
@@ -5,22 +5,26 @@ import { ref } from 'vue'
 const smallBanners = ref([
   {
     title: 'Apple AirPods <span>Max</span>',
+    alt: 'Apple AirPods Max',
     description: "Computational audio. Listen, it's powerful",
     image: 'apple_airpods_max.png'
   },
   {
     title: 'Apple Vision <span>Pro</span>',
+    alt: 'Apple Vision Pro',
     description: 'An immersive way to experience entertainment',
     image: 'apple_vision_pro.png'
   },
   {
     title: 'Playstation <span>5</span>',
+    alt: 'Playstation 5',
     description:
       'Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O will redefine your PlayStation experience.',
     image: 'playstation.png'
   },
   {
     title: 'Macbook <span>Air</span>',
+    alt: 'Macbook Air',
     description:
       'The new 15‑inch MacBook Air makes room for more of what you love with a spacious Liquid Retina display.',
     image: 'macbook_air.png',
@@ -42,7 +46,7 @@ function getImageUrl(image) {
     <template v-for="(banner, index) in smallBanners" :key="banner.image">
       <SmallBanner :class="`small-banner-item-${index + 1}`">
         <template #image>
-          <img :src="getImageUrl(banner.image)" :alt="banner.title" loading="lazy" />
+          <img :src="getImageUrl(banner.image)" :alt="banner.alt" loading="lazy" />
         </template>
         <template #title>
           <h2 v-html="banner.title"></h2>
@@ -62,9 +66,16 @@ function getImageUrl(image) {
   width: 100%;
 }
 
+.small-banner {
+  position: relative;
+  overflow: hidden;
+}
+
 @media (min-width: 576px) {
   .small-banner-btn {
     width: initial;
+    padding-left: 56px;
+    padding-right: 56px;
   }
 }
 
@@ -81,19 +92,123 @@ function getImageUrl(image) {
     grid-template-rows: repeat(2, 1fr);
     grid-column-gap: 0px;
     grid-row-gap: 0px;
+    max-height: 600px;
   }
 
-  .small-banner-item-3 {
-    grid-area: 1 / 1 / 2 / 3;
+  .small-banner {
+    padding: 3rem;
   }
+
+  .small-banner .small-banner-image {
+    position: absolute;
+  }
+
   .small-banner-item-1 {
     grid-area: 2 / 1 / 3 / 2;
   }
+
   .small-banner-item-2 {
     grid-area: 2 / 2 / 3 / 3;
   }
+  .small-banner-item-3 {
+    grid-area: 1 / 1 / 2 / 3;
+  }
   .small-banner-item-4 {
     grid-area: 1 / 3 / 3 / 5;
+  }
+
+  .small-banner-item-1 {
+    height: 272px;
+  }
+
+  .small-banner-item-2 {
+    height: 272px;
+  }
+
+  .small-banner-item-3 {
+    height: 328px;
+  }
+
+  .small-banner-item-4 {
+    height: 600px;
+  }
+
+  .small-banner-text-contents {
+    align-items: start;
+  }
+
+  .small-banner-description {
+    text-align: left;
+    font-size: 14px;
+  }
+
+  .small-banner-item-1 .small-banner-title h2,
+  .small-banner-item-2 .small-banner-title h2 {
+    font-size: 29px;
+  }
+
+  .small-banner-item-3 .small-banner-title h2 {
+    font-size: 49px;
+  }
+
+  .small-banner-item-4 .small-banner-title h2 {
+    font-size: 64px;
+  }
+
+  .small-banner-item-1 .small-banner-text-contents {
+    padding-left: 7.5vw;
+  }
+
+  .small-banner-item-2 .small-banner-text-contents {
+    padding-left: 8vw;
+  }
+
+  .small-banner-item-3 .small-banner-text-contents {
+    padding-left: 18vw;
+  }
+
+  .small-banner-item-4 .small-banner-text-contents {
+    padding-right: 16vw;
+  }
+
+  .small-banner-item-1 .small-banner-image img {
+    height: 272px;
+    max-height: 272px;
+  }
+
+  .small-banner-item-2 .small-banner-image img {
+    height: 190px;
+    max-height: 190px;
+  }
+
+  .small-banner-item-1 .small-banner-image {
+    left: -150px;
+  }
+
+  .small-banner-item-2 .small-banner-image {
+    left: -180px;
+  }
+
+  .small-banner-item-3 .small-banner-image {
+    left: -80px;
+  }
+
+  .small-banner-item-4 .small-banner-image {
+    right: -510px;
+  }
+
+  .small-banner-item-3 .small-banner-title h2 {
+    font-weight: 500;
+  }
+
+  .small-banner-item-3 .small-banner-image img {
+    height: 343px;
+    max-height: 343px;
+  }
+
+  .small-banner-item-4 .small-banner-image img {
+    height: 502px;
+    max-height: 502px;
   }
 }
 </style>
