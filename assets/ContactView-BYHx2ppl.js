4ce25f638230f1bc10f25a21be02c1afc4c86046
Janos Perge <janos.perge@gmail.com>
2024-11-06 17:19:21 +0100

    Updates

--- assets/ContactView-BYHx2ppl.js
+++
@@ -1,1 +0,0 @@
-import{U as t}from"./UnderConstruction-vvIPkxhv.js";import{c as a,a as o,b as s,o as r}from"./index-DE10VOnN.js";const n={class:"container py-16"},d={__name:"ContactView",setup(c){return(i,e)=>(r(),a("div",n,[e[0]||(e[0]=o("h1",{class:"page-head-title"},"Contact",-1)),s(t)]))}};export{d as default};
