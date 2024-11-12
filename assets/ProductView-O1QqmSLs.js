0e89491ff1abfcbb3093985ab9c22bddd2181373
Janos Perge <janos.perge@gmail.com>
2024-11-12 21:00:04 +0100

    Updates

--- assets/ProductView-O1QqmSLs.js
+++
@@ -1,1 +0,0 @@
-import{U as t}from"./UnderConstruction-CsMGyZoj.js";import{c as o,a,b as r,o as s}from"./index-CnNUVGAY.js";const c={class:"container py-16"},_={__name:"ProductView",setup(n){return(p,e)=>(s(),o("div",c,[e[0]||(e[0]=a("h1",{class:"page-head-title"},"Product page",-1)),r(t)]))}};export{_ as default};
