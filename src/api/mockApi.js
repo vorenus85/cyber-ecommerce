0bd5674c1add3e271b84e3bc7e85572eefb44453
Janos Perge <janos.perge@gmail.com>
2024-12-05 23:16:29 +0100

    create mockApi

---
+++ src/api/mockApi.js
@@ -0,0 +1,48 @@
+import products from '../../static/products.json' // Import the JSON file
+
+export const mockApi = {
+  getProducts: async (filters = {}, sort = null, page = 1, pageSize = 5) => {
+    // Simulate network latency (e.g., 1 to 3 seconds delay)
+    const latency = Math.floor(Math.random() * (3000 - 1000 + 1)) + 500 // Delay between 0.5 and 1.5 seconds
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        let result = [...products]
+
+        // Apply filtering
+        if (filters?.minPrice) {
+          result = result.filter(product => product.price >= filters.minPrice)
+        }
+        if (filters?.maxPrice) {
+          result = result.filter(product => product.price <= filters.maxPrice)
+        }
+        if (filters?.rating) {
+          result = result.filter(product => product.rating >= filters.rating)
+        }
+
+        // Apply sorting
+
+        if (sort) {
+          const [key, order] = sort.split('_')
+          result.sort((a, b) => {
+            if (order === 'asc') return a[key] - b[key]
+            if (order === 'desc') return b[key] - a[key]
+            return 0
+          })
+        }
+
+        // Paging
+        const startIndex = (page - 1) * pageSize
+        const endIndex = startIndex + pageSize
+        const paginatedResult = result.slice(startIndex, endIndex)
+
+        // Return the response after delay
+        resolve({
+          ok: 1,
+          total: result.length,
+          data: paginatedResult
+        })
+      }, latency) // Simulate the latency
+    })
+  }
+}
