e1b7fbbb24147657ba9d8c3eeccca91c2ef4a997
János Perge <janos.perge@gmail.com>
2026-02-01 14:35:00 +0100

    Updates

---
+++ assets/ContactView-BAc19-Y1.js
@@ -0,0 +1,1 @@
+import{U as t}from"./UnderConstruction-B3mmh36g.js";import{c as a,a as o,b as s,o as r}from"./index-CHaRW2dX.js";const n={class:"container py-16"},d={__name:"ContactView",setup(c){return(i,e)=>(r(),a("div",n,[e[0]||(e[0]=o("h1",{class:"page-head-title"},"Contact",-1)),s(t)]))}};export{d as default};
