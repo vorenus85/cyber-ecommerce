ba356184622c92e1780fc2076dee1e546fe9e797
János Perge <janos.perge@gmail.com>
2026-02-01 15:17:58 +0100

    Updates

--- assets/ProductView-SnKVZNal.js
+++
@@ -1,1 +0,0 @@
-import{U as t}from"./UnderConstruction-dFvqqzUq.js";import{c as o,a,b as r,o as s}from"./index-Cz_nc67Q.js";const c={class:"container py-16"},_={__name:"ProductView",setup(n){return(p,e)=>(s(),o("div",c,[e[0]||(e[0]=a("h1",{class:"page-head-title"},"Product page",-1)),r(t)]))}};export{_ as default};
